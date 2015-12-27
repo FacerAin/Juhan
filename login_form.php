7cbb51b6f03476317acc6a33a902f4abed745035
kimyeongrae <dudfo0629@gmail.com>
2015-12-27 11:59:04 +0900

    Update login_form.php
    좋았어

--- login_form.php
+++ login_form.php
@@ -7,7 +7,7 @@
 	<form action="login.php" method="post">
 		<div class="form-group">
 	    	<label for="form-title">아이디</label>
-	    	<input type="text" class="form-control" name="title" id="form-title" placeholder="Id">
+	    	<input type="text" class="form-control" name="name" id="form-title" placeholder="Id">
 	  	</div>
 	  	
 	    <div class="form-group">
@@ -15,7 +15,7 @@
 	        <input type="password" class="form-control" name="passowrd" id="form-password" placeholder="Password">
 	    </div>
 	    
-		<input type="submit" value="로그인" name="name" class="btn btn-primary">
+		<input type="submit" value="로그인"class="btn btn-primary">
 	</form>
 </body>
 </html>
